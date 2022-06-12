2b16230560e5ed67ef4f696c6ae175f2413faed9
itsberriver <jmejiaos713@gmail.com>
2022-06-12 18:43:25 +0200

    created manageChange, manageSubmit and addTask functions

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import '../stylesheets/TaskForm.css';
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function TaskForm(props){
+
+        const [inputText, setInputText] = useState('');
+ 
+        const manageChange = e =>{
+            setInputText(e.target.value);
+        }
+
+        const manageSubmit = e =>{
+
+            e.preventDefault(); 
+            const newTask = {
+                id: uuidv4(),
+                text: inputText,
+                done: false, 
+            }
+            props.onSubmit(newTask);
+        }
+
+ 
     return(
-        <form className='task-form'>
+        <form 
+        className='task-form'
+        onSubmit = {manageSubmit}>
             <input 
                 className='task-input'
                 type='text'
                 placeholder='add a task'
                 name='text'
+                onChange = {manageChange}
 
         />
         <button className='task-button'>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,23 +1,34 @@
 import React, { useState} from 'react';
 import '../stylesheets/TaskList.css';
 import TaskForm from '../components/TaskForm.jsx';
-import Task from '../components/Task.jsx';
+import Task from './Task.jsx';
+
 
 
 function TaskList(){
 
     const [tasks, setTasks] = useState([]);
 
+    const addTask = task =>{
+        if (task.text.trim()) {
+            task.text = task.text.trim();
+            const updatedTasks = [task,... tasks];
+            setTasks(updatedTasks);
+        }
+    }
+
     return(
         <>
-            <TaskForm />
+            <TaskForm onSubmit = {addTask} />
             <div className = 'task-list-container'>
             {
+                tasks.map((task) =>
                 <TaskForm 
-                text={task.text}
-                done={task.done}
+                    text={task.text}
+                    done={task.done}
                 />
-            }
+
+            )}
 
             </div>
         </>
